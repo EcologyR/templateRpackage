df3d777520a0c9a1f5f629545bf2015c24c11283
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-03-07 20:38:17 +0100

    add sum function

---
+++ R/suma.R
@@ -0,0 +1,17 @@
+#' Sumar valores numéricos
+#'
+#' Esta función suma valores numéricos.
+#'
+#' @param x Numeric
+#' @param y Numeric
+#'
+#' @return A number or numeric vector of the same length as x
+#' @export
+#'
+#' @examples
+#' suma(2, 3)
+#' suma(c(2, 2), c(3, 3))
+
+suma <- function(x = NULL, y = NULL) {
+  x + y
+}
